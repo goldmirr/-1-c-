801749c1d08597ec262fdb3c0731880bc3339464
goldmirr <kim_kyung@naver.com>
2021-03-11 16:32:50 +0900

    Add Cpp File [2WP1~5.cpp]

---
+++ 2WP1~5.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+using namespace std;
+/*
+1번 구구단
+int main() {
+	
+	
+	for (int i = 1; i < 10; i++) {
+		for (int j = 2; j < 6; j++) {
+			cout << j << " x " << i << " = " << i * j << "\t";
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	for (int i = 1; i < 10; i++) {
+		for (int j = 6; j < 10; j++) {
+			cout << j << " x " << i << " = " << i * j << "\t";
+		}
+		cout << endl;
+	}
+}
+*/
+/*
+2번문제 비밀번호 체크 프로그램
+int main() {
+	int a;
+	int b;
+	cout << "Please enter your pwd: ";
+	cin >> a;
+	cout << "Enter your pwd again: ";
+	cin >> b;
+
+	if (a == b) {
+		cout << "Correct pwd.";
+	}
+	else {
+		cout << "Incorrect pwd";
+	}
+}
+*/
+/*
+//문제 3 자료형 변환
+int main() {
+	int a;
+	int b;
+	cin >> a >> b;
+
+	cout << "a/b\t\t="  << a / b << endl;
+	cout << "a%b\t\t=" <<  a % b << endl;
+	cout << "a/(float)b\t="   << a / (float)b << endl;
+	cout << "a/(double)b\t="  << a / (double)b << endl;
+	
+	if( a>b){
+		cout << a << "와 " << b << " 중 큰 값\t"  << "=" << a;
+	}
+	else {
+		cout << a << "와 " << b << " 중 큰 값\t"  << "=" << b;
+	}
+}
+*/
+
+/*
+//4번 문제 자료형 최대값
+int main() {
+	int n=0;
+	int int_max=0;
+	int int_min=0;
+	while (n >= 0) {
+		int_max = n;
+		n++;
+		int_min = n;
+	}
+	
+	cout << "int overflow" << "\n";
+	cout << "maximum of int : " << int_max<<"\n";
+	cout << "minimum of int : " << int_min << endl;
+	
+}
+*/
+/*
+//5번 문제 자료형 사이즈 byte로 출력됨
+int main() {
+	int i;
+	double d;
+	char c;
+	float f;
+	short s;
+	cout << sizeof(i) << endl;
+	cout << sizeof(d) << endl;
+	cout << sizeof(c) << endl;
+	cout << sizeof(f) << endl;
+	cout << sizeof(s) << endl;
+}
+*/
+
+
+
+
+
+
